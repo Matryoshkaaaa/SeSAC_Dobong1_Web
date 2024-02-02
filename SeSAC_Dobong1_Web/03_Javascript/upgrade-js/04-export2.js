9fff6f6b9a615115566ee48aab93192ddcaab670
Matryoshkaaaa <154948725+Matryoshkaaaa@users.noreply.github.com>
2024-02-02 16:34:59 +0900

    Delete SeSAC_Dobong1_Web directory
    Delete SeSAC_Dobong1_Web directory

--- SeSAC_Dobong1_Web/03_Javascript/upgrade-js/04-export2.js
+++
@@ -1,4 +0,0 @@
-export const animals = ["dog", "cat"];
-export function showAnimals() {
-  animals.forEach((el) => console.log(el));
-}
--- SeSAC_Dobong1_Web/03_Javascript/upgrade-js/05_exportdefault.js
+++
@@ -1,3 +0,0 @@
-export default function sayHi() {
-  console.log("export default 사용했습니다!!!");
-}
